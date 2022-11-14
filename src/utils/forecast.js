331f8871c4b538ca1c70b9e5e22f51097b638e3a
Filipe Miguel Gamboa Ferreira <filipe.ferreira@aitec.pt>
2022-11-14 10:08:06 +0000

    Change forecast output to include humidity info

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -12,7 +12,7 @@ const forecast = (lat, long, callback) => {
       const data = body.current;
       callback(
         undefined,
-        `${data.weather_descriptions[0]}. It is currently ${data.temperature} degrees out. It feels like ${data.feelslike} degrees out.`
+        `${data.weather_descriptions[0]}. It is currently ${data.temperature} degrees out. It feels like ${data.feelslike} degrees out. The humidity is ${data.humidity}%.`
       );
     }
   });
